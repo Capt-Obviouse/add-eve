bf88b43d050bc4443b9cf656f64cf2a4b4b312c9
Capt-Obviouse <sixstringjesse@gmail.com>
2016-05-04 11:22:39 -0700

    new push

---
+++ app/controllers/eve_controller.rb
@@ -0,0 +1,11 @@
+class EveController < ApplicationController
+ 	def status
+ 		key_id = 4401623
+		v_code = '2WPjGh2aleUh9c8ySkG0Ip3yY6uSYUFQTADmXWHzgdJ7Zurc9ygrdw8YzkwAOQCd'
+		@account_status = EveOnline::Account::Status.new(key_id, v_code)
+
+		@characters = EveOnline::Account::Characters.new(key_id, v_code)
+		@characters = @characters.characters.second
+
+ 	end
+end
---
+++ app/helpers/eve_helper.rb
@@ -0,0 +1,2 @@
+module EveHelper
+end
