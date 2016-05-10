ed81b8ef3ba4b451b94b9d4a6a283ec70fc49648
Capt-Obviouse <sixstringjesse@gmail.com>
2016-05-09 21:58:52 -0700

    save updates for the night

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508191005) do
+ActiveRecord::Schema.define(version: 20160510043158) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 191, null: false
@@ -227,6 +227,7 @@ ActiveRecord::Schema.define(version: 20160508191005) do
     t.string   "last_successful_api_call"
     t.string   "admin"
     t.string   "users"
+    t.integer  "rank"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
