89594905d2a39e0f52e178b542bd74490d87693a
Capt-Obviouse <sixstringjesse@gmail.com>
2016-05-27 12:01:57 -0700

    working on mentors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527174040) do
+ActiveRecord::Schema.define(version: 20160527180340) do
 
   create_table "characters", force: :cascade do |t|
     t.integer "user_id"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20160527174040) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "mentors", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "mentors", ["user_id"], name: "index_mentors_on_user_id"
 
   create_table "news", force: :cascade do |t|
     t.string   "title"
